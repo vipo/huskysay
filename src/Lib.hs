2a018a20e06cc8530effd8b4edb63ec9f6d03ff7
Viačeslav Pozdniakov <viaceslav.pozdniakov@adform.com>
2016-05-18 14:56:33 +0300

    Cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,20 +2,20 @@ module Lib where
 
 import Types
 import Defaults
-
 import qualified Data.List.Split as S
 
 -- | Renders a given text as ascii art
 render :: String   -- ^ String to render
-       -> Font     -- ^ Font to use       
-       -> [String] -- ^ Lines of rendered text 
+       -> Font     -- ^ Font to use
+       -> [String] -- ^ Lines of rendered text
 render txt font = 
-  S.chunksOf (size font * length txt) $ concatSegments (map findChar txt)
-                 ""
+  chunk (concatSegments (map findChar txt)
+                        "")
   where findChar c = 
           case lookup c (mapping font) of
             Nothing -> error $ "No data for char: " ++ [c]
             Just m -> m
+        chunk = S.chunksOf (size font * length txt)
         poinToChar p = 
           case p of
             W -> ' '
